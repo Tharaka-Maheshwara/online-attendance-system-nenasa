6ecd9eea812f044c3f60a69ce8d8c2f198121223
Tharaka-Maheshwara <159180873+Tharaka-Maheshwara@users.noreply.github.com>
2025-09-07 09:18:08 +0530

    Backend setup

---
+++ Backend/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { RoleModule } from './role/role.module';
+import { ClassModule } from './class/class.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Thara@PSQL',
+      database: 'attendance_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    RoleModule,
+    ClassModule,
+    AttendanceModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
